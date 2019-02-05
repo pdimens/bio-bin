c5210d5108ad7f5f656a745e6587a0ab005c4d39
Pavel V. Dimens <pdimens@live.com>
2019-02-05 15:59:23 -0600

    can now find-replace readname text

--- fasta_readnames.jl
+++ fasta_readnames.jl
@@ -1,37 +1,43 @@
 #! /usr/bin/env julia
 
 if length(ARGS)==0
-  print("This julia script will pull out all the read names from a fasta file",
-  "\n", "and output them into readnames_from_<filename>","\n",
-  "[usage] fasta_readnames <fasta file>")
+  print("This julia script can pull out all the read names from fasta files",
+        "\n", "or it can do a find-replace of readnames", "\n","\n",
+  "[usage1] fasta_readnames <fasta file(s)>      # to pull out all readnames","\n",
+  "[usage2] fasta_readnames rename <fasta file> <find.this> <replace.with.this>")
 exit()
 end
+
 using BioSequences
 
-function outputnames()
-    for eachfile in ARGS
-  inputfasta = FASTA.Reader(open(eachfile,"r"))
-  outfile = open("readnames_from_" * basename(eachfile),"w")
-  for eachread in inputfasta
-    write(outfile, BioSequences.FASTA.identifier(eachread),"\n")
-end
-  close(inputfasta)
-  close(outfile)
-end
-end
-outputnames()
+if ARGS[1]!="rename"
+  function outputnames()
+      for eachfile in ARGS
+        inputfasta = FASTA.Reader(open(eachfile,"r"))
+        outfile = open("readnames_from_" * basename(eachfile),"w")
+        for eachread in inputfasta
+          write(outfile, BioSequences.FASTA.identifier(eachread),"\n")
+        end
+        close(inputfasta)
+        close(outfile)
+      end
+  end
+  outputnames()
+  print("Your readnames file(s) are prefixed with readnames_from_")
 
+  elseif ARGS[1]=="rename"
+  function alter_readnames()
+    inputfasta = FASTA.Reader(open(ARGS[2],"r"))
+    outfile = FASTA.Writer(open(ARGS[2] * "_renamed", "w"))
+    for eachread in inputfasta
+      readname = BioSequences.FASTA.identifier(eachread)
+      new_read = BioSequences.FASTA.Record(replace(readname,ARGS[3] => ARGS[4]),BioSequences.FASTA.sequence(eachread))
+      write(outfile,new_read)
+    end
+    close(inputfasta)
+    close(outfile)
+  end
 
-#= future search functionality
-function alter_readnames()
-inputfasta = FASTA.Reader(open(ARGS[1],"r"))
-outfile = FASTA.Writer(open(basename(ARGS[1]) * "_renamed", "w"))
-  for eachread in inputfasta
-    readname = BioSequences.FASTA.identifier(eachread)
-    new_readname = replace(readname,ARGS[2] => ARGS[3])
-    write(outfile, FASTA.Record(new_readname,eachread.sequence),"\n")
+  alter_readnames()
+  print("Your read-renamed fasta file is named ", ARGS[2] * "_renamed")
 end
-  close(inputfasta)
-  close(outfile)
-end
-=#
