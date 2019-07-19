6113c7256453b2e59599f060fddd54bb05c593b7
Pavel V. Dimens <pdimens@live.com>
2019-07-19 13:16:42 -0500

    julian cleaning

--- CountSeq.jl
+++ CountSeq.jl
@@ -8,44 +8,52 @@ if length(ARGS)==0
   exit()
 end
 
-using BioSequences
-using GZip
-using Base.Threads
+using BioSequences, GZip, Base.Threads
 
-function getcounts()
-    @threads for infile in ARGS
-    if occursin(".fastq", lowercase(infile)) | occursin(".fq", lowercase(infile))
-      if occursin(".gz", lowercase(infile))
-        seqfile= FASTQ.Reader(GZip.open(infile,"r"))
-      else
-        seqfile= FASTQ.Reader(open(infile,"r"))
-      end
-      filetype="fastq"
-    elseif occursin(".fasta", lowercase(infile)) | occursin(".fa", lowercase(infile)) | occursin(".txt", lowercase(infile))
-      if occursin(".gz", lowercase(infile))
-        seqfile= FASTA.Reader(GZip.open(infile,"r"))
-      else
-        seqfile= FASTA.Reader(open(infile,"r"))
-      end
-      filetype="fasta"
-    else
-      println("$infile format incorrect, must be .fa .fasta .txt .fq .fastq or gzipped versions")
-      continue
-    end
-    reads = 0
-    bp = 0
-    for record in seqfile
-      reads += 1
-      if filetype == "fasta"
-        bp += length(BioSequences.FASTA.sequence(record))
-      elseif filetype == "fastq"
-        bp += length(BioSequences.FASTQ.sequence(record))
-      end
-    end
-    println(infile, "\t", reads, "\t", bp)
-    close(seqfile)
+function getcountsfq(fastx::String)
+  if occursin(".gz", lowercase(fastx))
+    seqfile= FASTQ.Reader(GZip.open(fastx,"r"))
+  else
+    seqfile= FASTQ.Reader(open(fastx,"r"))
   end
+  reads = 0
+  bp = 0
+  for record in seqfile
+    reads += 1
+    bp += length(BioSequences.FASTQ.sequence(record))
+  end
+    println(fastx, "\t", reads, "\t", bp)
+    close(seqfile)
+end
+
+function getcountsfa(fastx::String)
+  if occursin(".gz", lowercase(fastx))
+    seqfile= FASTA.Reader(GZip.open(fastx,"r"))
+  else
+    seqfile= FASTA.Reader(open(fastx,"r"))
+  end
+  reads = 0
+  bp = 0
+  for record in seqfile
+    reads += 1
+    bp += length(BioSequences.FASTA.sequence(record))
+  end
+  println(fastx, "\t", reads, "\t", bp)
+  close(seqfile)
 end
 
 println("filename", "\t", "#reads", "\t", "#basepairs")
-getcounts()
+
+function runcounts(infiles::Array{String,1})
+  @threads for infile in infiles
+    if occursin(".fastq", lowercase(infile)) | occursin(".fq", lowercase(infile))
+      getcountsfq(infile)
+    elseif occursin(".fasta", lowercase(infile)) | occursin(".fa", lowercase(infile)) | occursin(".txt", lowercase(infile))
+      getcountsfa(infile)
+    else
+        println("$infile format incorrect, must be .fa .fasta .txt .fq .fastq or gzipped versions")
+        continue
+    end
+  end
+end
+runcounts(ARGS)
