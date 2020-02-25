a4c813fd9a4e428d3f44217178a0882c243c6ee4
Pavel V. Dimens <pdimens@live.com>
2020-02-24 20:01:39 -0600

    Create FastStructureK.sh

---
+++ FastStructureK.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+
+if [[ -z "$1" ]]; then
+  echo "Run a series of fastSTRUCTURE for a range of 1 to K values"
+  echo "[usage] FastStructureK.sh infile Kmax"
+  echo "- the input file must end in .str"
+  exit 1
+fi
+INRAW=$1
+INFILE="${INRAW%.str}" 
+KVAL=$2
+
+for i in $(seq 1 $KVAL);
+do
+echo "Running fastSTRUCTURE for K = $i"
+structure.py \
+    -K $i \
+    --format=str \
+    --input=$INFILE \
+    --output=${INFILE}_out
+done
+
+# create a summary file
+for i in $(seq 1 $KVAL);
+do
+	echo -e "\nK = $i :\t" >> fastStructure.summary
+	grep 'Marginal Likelihood =' ${INFILE}_out.$i.log >> ${INFILE}_fastStructure.summary
+done
+
+echo "Done!"
+echo "See output summary in ${INFILE}_fastStructure.summary"
