f2f637c0f69db452bba1ad1c28c430ce62216448
Pavel V. Dimens <pdimens@live.com>
2020-04-23 16:58:57 -0500

    smaller and faster
    removed DataFrames dependency, replaced pairwise method with faster and lighter `pairwise` functions.

--- CountMismatch.jl
+++ CountMismatch.jl
@@ -1,45 +1,44 @@
 #! /usr/bin/env julia
 
-using DataFrames, PlotlyJS, CSV ;
+using PlotlyJS, CSV ;
 
-function countmis2(infile)
+function pairwise(x::String, y::String = x, op = ==)
+    broadcast(op, Iterators.Stateful(x), Iterators.Stateful(y))
+end
+
+function pairwise(x::Array{String,1}, y::Array{String,1} = x, op = ==)
+    sum.(pairwise.(x, permutedims(y), op))
+end
+
+function countmatch(infile)
     indexfile = open(readlines,infile)
-    mismatchArray =  []
-    mismatchNames = Symbol.(indexfile)
-    for each in indexfile 
-        tempcol = Array{Int64,1}()
-        for other in indexfile
-            push!(tempcol, 
-                  count(split(each,"") .!= split(other,""))
-                  )
-        end
-        push!(mismatchArray, tempcol)
-    end
-    df = DataFrame(mismatchArray, mismatchNames)
-    insertcols!(df, 1, :index => indexfile)
-    CSV.write("MismatchArray.txt", df)
-    layout = Layout(title = "Mismatches",
-                    margin = attr(l=100,
-                                  r=100,
-                                  b=100,
-                                  t=100,
-                                  pad=0
-                                 )
-                    ) 
+    mismatchArray = pairwise(indexfile)
+    outfile_array = vcat(permutedims(["string";indexfile]), hcat(indexfile,mismatchArray))
+    CSV.write("PairwiseMatch.txt", outfile_array)
+    layout = Layout(
+                title = "Matches",
+                margin = attr(
+                            l=100,
+                            r=100,
+                            b=100,
+                            t=100,
+                            pad=0
+                    )
+            )
     p = plot(
             heatmap(
-                x = indexfile, 
-                y = indexfile, 
+                x = indexfile,
+                y = indexfile,
                 z=mismatchArray,
                 colorscale = :YlGnBu,
-                #reversescale = true
+                reversescale = true
                 ),
             layout
-            )
-    PlotlyJS.savehtml(
-                p,
-                split(ARGS[1], ".")[1]*".mismatch.html",
-                :embed
         )
+    PlotlyJS.savehtml(
+        p,
+        split(ARGS[1], ".")[1]*".PairwiseMatch.html",
+        :embed
+    )
 end
-countmis2(ARGS[1])
+countmatch(ARGS[1])
