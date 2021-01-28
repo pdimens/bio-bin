43bec26f738b63d9a9a59a7d04787fa21d9404df
Pavel V. Dimens <pdimens@live.com>
2021-01-28 11:26:00 -0600

    update to use facets
    This rework of the script now includes:
    - usage text when run without an argument and the .str file isnt in the working directory
    - uses ggplot's `facets` instead of gridextra (one less dep)
    - output grid is now 1-column and k-rows for better visibility
    - consolidated population name text to the very bottom plot since it's all vertically aligned
    - population division lines now dashed

--- FastStructurePlot.R
+++ FastStructurePlot.R
@@ -1,17 +1,28 @@
+#! /usr/bin/env Rscript
+
 args <- commandArgs(trailingOnly = TRUE)
 
 library(ggplot2)
 library(tidyr)
-library(gridExtra)
 library(data.table)
+
 setwd(getwd())
+
+if (length(args) == 0 && length(list.files(pattern = "\\.str$")) == 0){
+    cat("\nCreates data exploration plots from FastStructureK.sh results.")
+    cat("\nAssumes the fastStructure input file (\"__.str\") is in the working dir. \nIf it's not, then supply its path/name as the only argument")
+    cat("\n\n[usage] FastStructurePlot.r   or    FastStructurePlot.r ../data/somefile.str")
+    q()
+}
 
 ## pattern match to find output files from FaststructureK.sh
 infiles <- list.files(pattern = "[2-9][0-9]?.meanQ")
+
 ## pull out basename to regenerate original structure filename
 
 ## load in all the q values
 q_scores <- lapply(infiles,fread)
+
 ## load in fs file and pull out sample names from the first column
 f_basename <- strsplit(infiles[1], "_out")[[1]][1]
 if (length(args) == 0) {
@@ -19,13 +30,31 @@ if (length(args) == 0) {
 } else {
     samp_names <- as.character(unlist(unique(fread(args[1])[,1])))
 }
+
 ## correct any hyphens in sample names to underscores
 samp_names <- gsub("-", "_", samp_names)
 
-# add names column to each
+## instantiate empty DF to be our awesome long-format table
+tidy_data <- data.frame(
+    matrix(vector(), 0, 4,
+    dimnames=list(c(), c("k", "sample", "population", "value"))),
+    stringsAsFactors=TRUE
+)
+
 for (i in 1:length(q_scores)){
-    q_scores[i][[1]]$sample = samp_names
+    # drop the "V" in the columns names
+    colnames(q_scores[[i]]) <- gsub("^[V]", "", colnames(q_scores[[i]]))
+    # add a K column
+    q_scores[[i]]$k <- as.factor(length(colnames(q_scores[[i]])))
+    # add a names column
+    q_scores[[i]]$sample <- samp_names
+    # wide-to-long format & append
+    tidy_data <- rbind(
+        tidy_data,
+        pivot_longer(q_scores[[i]], cols = !c(k, sample), names_to = "population")    
+    ) 
 }
+
 
 # values for population annotations
 ## derive population names from sample names
@@ -49,42 +78,42 @@ poptext <- c(0,poplines$freq)
 for(i in 1:8){
         poptext[i] <- poptext[i] + (poptext[i+1] -  poptext[i])/2
 }
+
 #### trim off last value
 poptext <- poptext[1:length(poptext)-1]
 poplines <- head(poplines, -1)
 
-# stack each into long format
-for (i in 1:length(q_scores)){
-    q_scores[i][[1]] <- pivot_longer(q_scores[i][[1]], cols = !(dim(q_scores[i][[1]])[2]))
-    colnames(q_scores[i][[1]]) <- c("sample", "population", "probability")
-    tmp <- strsplit(q_scores[i][[1]]$population, "V")
-    q_scores[i][[1]]$population <- as.factor(sapply(tmp, function(x){x[2]}))
-}
+popdata_df <- data.frame(
+    xposition = poptext, 
+    yposition = -0.2, 
+    population = levels(popnames), 
+    k = factor(max(as.numeric(levels(tidy_data$k)), levels(tidy_data$k)))
+    )
 
-plots <- list()
-for (i in 1:length(q_scores)) {
-   str_plot <- ggplot(q_scores[i][[1]], aes_string(fill = "population", y = "probability", x = "sample")) +
-            ylab("Probability of Membership") + 
-            geom_bar(position = "fill", stat = "identity", width = 1.0) +
-            geom_vline(xintercept = poplines$freq, size = 0.4, color = "#000000") +
-            coord_cartesian(ylim = c(0, 1), expand = FALSE, clip = "off") +
-            annotate("text", x = poptext, y = 0.1, label = levels(popnames)) +
-            theme(
-                axis.title = element_text(size=14),
-                axis.title.x = element_blank(),
-                axis.text.x = element_blank(),
-                axis.ticks.x = element_blank(),
-                panel.grid.major = element_blank(),
-                panel.background = element_blank(),
-                panel.grid.minor = element_blank()
-            )
-   plots[[i]] <- str_plot
-}
-
+p <- ggplot(tidy_data, aes_string(fill = "population", y = "value", x = "sample")) +
+    ylab("Probability of Membership") +
+    geom_bar(position = "fill", stat = "identity", width = 1) +
+    geom_vline(xintercept = poplines$freq, size = 0.4, color = "#000000", linetype = "dashed") +
+    coord_cartesian(ylim = c(0, 1), expand = FALSE, clip = "off") +
+    labs(title = "fastStructure probabilities for values of K") +
+    facet_wrap(~ k, ncol = 1,labeller = labeller(k = label_both), strip.position = "right") +
+    geom_text(data = popdata_df, inherit.aes = FALSE, aes(x = xposition, y = yposition, label = population), show.legend = FALSE) +
+    theme(
+        axis.title = element_text(size=12),
+        axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        strip.text = element_text(size = 14),
+        panel.grid.major = element_blank(),
+        panel.background = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.spacing.y = unit(1.5, "lines"),
+        plot.margin = unit(c(5.5, 5.5, 30, 5.5), "pt")
+    )
 
 plt_h <- length(q_scores) %% 3 * 5
 pdf(file = paste0(f_basename, ".pdf"), 24, plt_h)
-do.call(grid.arrange,plots)
+p
 .x <- dev.off()
 
 q()
