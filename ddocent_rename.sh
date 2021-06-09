fc76dd05555a65e61eccd92033329e30c4b3b0da
Pavel V. Dimens <pdimens@live.com>
2021-06-09 10:06:16 -0400

    Update ddocent_rename.sh

--- ddocent_rename.sh
+++ ddocent_rename.sh
@@ -1,15 +1,15 @@
 #! /usr/bin/env bash
 
-echo "Renaming forward \"R1\" reads. "
+echo -n "Renaming forward \"R1\" reads. "
 for i in *_R1_*.gz; do
     NAME=$(echo $i | cut -d"_" -f1,2)
     mv $i $NAME.F.fq.gz
 done
-echo -n "Done!"
+echo "Done!"
 
-echo "Renaming reverse \"R2\" reads. "
+echo -n "Renaming reverse \"R2\" reads. "
 for i in *_R2_*.gz; do
     NAME=$(echo $i | cut -d"_" -f1,2)
-    mv $i $i.R.fq.gz
+    mv $i $NAME.R.fq.gz
 done
-echo -n "Done!"
+echo "Done!"
