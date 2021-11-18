281e201ee2eff6b44b0c51b2921d74efebadbfd5
Pavel V. Dimens <pdimens@live.com>
2021-11-18 16:53:35 -0500

    Update setup_environment.m

--- misc/setup_environment.m
+++ misc/setup_environment.m
@@ -1,11 +1,17 @@
 function setup_environment()
+  disp("Downloading and decompressing course materials")
   websave("MAE101.zip","https://ecornell.s3.amazonaws.com/content/MAE/MAE101/matlabDeploy/MAE101.zip");
-	unzip("MAE101.zip");
-	addpath(genpath("MAE101"));
-	savepath;
-	websave("simulator.zip","https://ecornell.s3.amazonaws.com/content/MAE/MAE100/simulator.zip");
+  unzip("MAE101.zip");
+  addpath(genpath("MAE101"));
+  savepath;
+  delete MAE101.zip
+  disp("Downloading and decompressing simulator")
+  websave("simulator.zip","https://ecornell.s3.amazonaws.com/content/MAE/MAE100/simulator.zip");
   unzip("simulator.zip");
-	addpath(genpath("simulator"));
-	savepath;
-	matlab.addons.toolbox.installToolbox("Simulator/CreateRobotSimulator.mltbx");
+  addpath(genpath("simulator"));
+  savepath; 
+  delete simulator.zip
+  disp("Installing Robot Simulator Toolbox")
+  matlab.addons.toolbox.installToolbox("Simulator/CreateRobotSimulator.mltbx");
+  delete setup_environment.m
 end
