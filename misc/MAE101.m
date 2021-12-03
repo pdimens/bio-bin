0545a258c443db94256572b3489665ccdc5359fe
Pavel V. Dimens <pdimens@live.com>
2021-12-03 15:53:28 -0500

    Update MAE101.m

--- misc/MAE101.m
+++ misc/MAE101.m
@@ -8,6 +8,34 @@ function MAE101(exercise)
     movefile .MAE101.tmp/MAE101 .MAE101 ;
     rmdir .MAE101.tmp s
   end
+  exer = char(exercise) ;
+  if lower(exer) == "simulator"
+    if isfolder('Simulator')
+        prompt = append('Replace existing Simulator folder? (yes/no)');
+        answer = input(prompt,'s');
+        if lower(answer) ==  "yes"
+            disp("Downloading and decompressing simulator")
+            websave(".Simulator.zip","https://ecornell.s3.amazonaws.com/content/MAE/MAE100/simulator.zip");
+            unzip(".Simulator.zip", ".Simulator");
+            movefile .Simulator/Simulator Simulator
+            rmdir .Simulator s
+            addpath(genpath("Simulator"));
+            savepath; 
+            delete .Simulator.zip
+        end
+    else
+        disp("Downloading and decompressing simulator")
+        websave(".Simulator.zip","https://ecornell.s3.amazonaws.com/content/MAE/MAE100/simulator.zip");
+        unzip(".Simulator.zip", ".Simulator");
+        movefile .Simulator/Simulator Simulator
+        rmdir .Simulator s
+        addpath(genpath("Simulator"));
+        savepath; 
+        delete .Simulator.zip
+    end
+    disp("Installing Robot Simulator Toolbox")
+    matlab.addons.toolbox.installToolbox("Simulator/CreateRobotSimulator.mltbx");
+  end
   if not(isfolder('Simulator'))
     disp("Downloading and decompressing simulator")
     websave(".Simulator.zip","https://ecornell.s3.amazonaws.com/content/MAE/MAE100/simulator.zip");
@@ -36,31 +64,14 @@ function MAE101(exercise)
     savepath; 
   end
   exer = char(exercise) ;
-  if lower(exer) == "simulator"
-        if isfolder('Simulator')
-            prompt = append('Replace existing Simulator folder? (yes/no)');
-            answer = input(prompt,'s');
-            if answer ==  "yes"
-                disp("Downloading and decompressing simulator")
-                websave(".Simulator.zip","https://ecornell.s3.amazonaws.com/content/MAE/MAE100/simulator.zip");
-                unzip(".Simulator.zip", ".Simulator");
-                movefile .Simulator/Simulator Simulator
-                rmdir .Simulator s
-                addpath(genpath("Simulator"));
-                savepath; 
-                delete .Simulator.zip
-            end
-        end
-    disp("Installing Robot Simulator Toolbox")
-    matlab.addons.toolbox.installToolbox("Simulator/CreateRobotSimulator.mltbx");
-  elseif not(isfolder(exer))
+  if not(isfolder(exer))
     copyfile(append('.MAE101/', exer), exer) ;
     addpath(genpath(exer)) ;
     savepath ;
   else
       prompt = append('Replace existing "', exer, 'folder? (yes/no)');
       answer = input(prompt,'s');
-      if answer ==  "yes"
+      if lower(answer) ==  "yes"
         copyfile(append('.MAE101/', exer), exer, 'f') ;
         addpath(genpath(exer)) ;
         savepath ;
