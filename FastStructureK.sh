e64b12f88329a9a87ffcf979cb706e8dafc33e18
Pavel V. Dimens <pdimens@live.com>
2022-07-27 13:50:13 -0400

    Update FastStructureK.sh

--- FastStructureK.sh
+++ FastStructureK.sh
@@ -1,40 +1,35 @@
  #! /usr/bin/env bash
 
 if [[ -z "$1" ]]; then
-  echo "Run a series of fastSTRUCTURE for a range of 1 to K values"
-  echo "[usage] FastStructureK.sh infile Kmax"
-  echo "- the input file must end in .str"
+  echo "Run a series of fastSTRUCTURE for a range of Kstart to Kmax values"
+  echo "[usage] FastStructureK.sh infile Kstart Kmax"
+  echo "  - the input file must end in .str"
   exit 1
 fi
 INRAW=$1
 INFILE="${INRAW%.str}"
-OUTBASE=$(basename $INFILE)
-KVAL=$2
-echo "#fastSTRUCTURE likelihoods for $INRAW" > ${OUTBASE}.fs.summary
-echo -ne "k likelihood\n" >> ${OUTBASE}.fs.summary
+KSTART=$2
+KVAL=$3
+echo "#fastSTRUCTURE likelihoods for $INRAW" > ${INFILE}.fs.summary
+echo -ne "k likelihood\n" >> ${INFILE}.fs.summary
 
 export INRAW
 export INFILE
-export OUTBASE
 export KVAL
 
 fstruct(){
 	echo "Running fastSTRUCTURE for K = $1"
-	echo "Output file: ${OUTBASE}"
+	mkdir -p k$1
 	python $(which structure.py) \
 	-K $1 \
 	--format=str \
 	--input=$INFILE \
-	--output=${OUTBASE} \
-	--cv=30
-	grep 'Marginal Likelihood =' ${OUTBASE}.$1.log
+	--output=k$1/${INFILE}_out
+	grep 'Marginal Likelihood =' k$1/${INFILE}_out.$1.log
+	# add to summary file
+	echo -ne "$1 " >> ${INFILE}.fs.summary
+	grep 'Marginal Likelihood =' k$1/${INFILE}_out.$1.log | cut -d"=" -f2 >> ${INFILE}.fs.summary
 }
 export -f fstruct
 
-parallel --jobs $(nproc) fstruct {1} ::: $(seq 1 $KVAL)
-
-# add to summary file
-for K in $(seq 1 $KVAL) ; do
-	echo -ne $K >> ${OUTBASE}.fs.summary
-	grep 'Marginal Likelihood =' ${OUTBASE}.$K.log | cut -d"=" -f2 >> ${OUTBASE}.fs.summary
-done
+parallel --jobs $(nproc) fstruct {1} ::: $(seq $KSTART $KVAL)
